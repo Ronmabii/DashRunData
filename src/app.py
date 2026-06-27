e1dd452c98f8e2afc34df9b56a33c09be18c9f05
Ronnie <ronmabii@gmail.com>
2026-06-27 14:59:36 -0400

    can switch between charts now

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Output,Input,ctx
 import pandas as pd
 from pathlib import Path
 import plotly.express as px
@@ -13,62 +13,37 @@ df = pd.read_csv(csvPath)
 df[["Date","Week (Monday)"]] = df[["Date","Week (Monday)"]].apply(pd.to_datetime) # needs this to be recognized as a continuous timeline, otherwise compressed(str to datetime)
 
 weeklyMileageSum = (
-    df.groupby("Week (Monday)", as_index=False)["Miles"] # without as_index wrong column entered into fig
+    df.groupby("Week (Monday)", as_index=False)["Miles"] # dff["Miles"].sum() without as_index wrong column entered into fig
       .sum()
 )
 
-fig = px.bar(
-    weeklyMileageSum,
-    x="Week (Monday)",
-    y="Miles",
-    labels={
-        "Week (Monday)": "Weeks",
-        "Miles": "Total Miles"
-    },
-    title="Weekly Running Mileage"
-)
-
-# fig = px.scatter(df,x='Date',y='Miles',title="Running Timeline") # daily dots
-
 app.layout = html.Div(
     [
-        html.H1("Big Head"),
-        dcc.Graph(id='chart', figure = fig)
+        html.H1("Running Data"),
+        html.Button("Overall", id="btn-overall"),
+        html.Button("Weekly", id="btn-week"),
+        dcc.Graph(id="chart", figure = {}, style={"width": "100%", "height": "80vh"})
     ]
 )
 
+@callback(
+    Output("chart", "figure"),
+    [
+        Input("btn-overall", "n_clicks"),
+        Input("btn-week", "n_clicks"),
+    ],
+)
+def func(n_clicks_btn1, n_clicks_btn2):
+    button_id = ctx.triggered_id
+
+    if button_id == "btn-week":
+        fig = px.bar(weeklyMileageSum, x="Week (Monday)",y="Miles",labels={"Week (Monday)": "Weeks"},title="Weekly Running Mileage")
+    else:
+        fig = px.scatter(df,x='Date',y='Miles',title="Running Timeline")
+
+    return fig
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-# from dash import Dash, html, Input, Output, callback, ctx
-
-# app = Dash()
-
-# app.layout = html.Div(
-#     [
-#         html.Button("Button 1", id="btn-1"),
-#         html.Button("Button 2", id="btn-2"),
-#         html.Div(id="output-div"),
-#     ]
-# )
-
-
-# @callback(
-#     Output("output-div", "children"),
-#     [
-#         Input("btn-1", "n_clicks"),
-#         Input("btn-2", "n_clicks"),
-#     ],
-# )
-# def func(n_clicks_btn1, n_clicks_btn2):
-#     button_id = ctx.triggered_id
-#     message = f"You've clicked {button_id} last" if button_id else "No clicks yet"
-#     return message
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
