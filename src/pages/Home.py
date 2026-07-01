25842c9c415d9d913a4de4f7d7c869dd5b5c46aa
Ronnie <ronmabii@gmail.com>
2026-07-01 16:39:53 -0400

    pace chart

--- src/pages/Home.py
+++ src/pages/Home.py
@@ -12,7 +12,15 @@ df = pd.read_csv(csvPath)
 
 df[["Date","Week (Monday)"]] = df[["Date","Week (Monday)"]].apply(pd.to_datetime) # needs this to be recognized as a continuous timeline, otherwise compressed(str to datetime)
 df['Duration(Mins)'] = pd.to_timedelta(df['Duration(Mins)']) 
-df["Minutes"] = df["Duration(Mins)"].dt.total_seconds() /60# would be easier to convert to seconds in csv
+df["Minutes"] = df["Duration(Mins)"].dt.total_seconds() /60 # would be easier to convert to seconds in csv
+
+def pace_to_numeric(pace): # keep 00:10:30 min/mile format into floats (10.5) next time for easier time
+    if pd.isna(pace):
+        return pace
+    minutes, seconds = map(int, pace.split(':'))
+    return minutes + (seconds / 60.0)
+
+df["Average Pace"] = df["Average Pace"].apply(pace_to_numeric)
 
 weeklyMileageSum = (
     df.groupby("Week (Monday)", as_index=False)["Miles"] # dff["Miles"].sum() without as_index wrong column entered into fig
@@ -27,6 +35,7 @@ layout = html.Div(
         html.Button("Weekly Mileage", id="btn-week"),
         html.Button("Heart Rate", id="btn-heart"),
         html.Button("Time", id="btn-duration"),
+        html.Button("Pace", id="btn-pace"),
         dcc.Graph(id="chart", figure = {}, style={"width": "100%", "height": "80vh"})
     ]
 )
@@ -39,9 +48,10 @@ layout = html.Div(
         Input("btn-week", "n_clicks"),
         Input("btn-heart", "n_clicks"),
         Input("btn-duration", "n_clicks"),
+        Input("btn-pace", "n_clicks"),
     ],
 )
-def func(n_clicks_btn1, n_clicks_btn2,n_clicks_btn3,n_clicks_btn4):
+def func(n_clicks_btn1, n_clicks_btn2,n_clicks_btn3,n_clicks_btn4,n_clicks_btn5):
     button_id = ctx.triggered_id
 
     if button_id == "btn-week":
@@ -51,6 +61,9 @@ def func(n_clicks_btn1, n_clicks_btn2,n_clicks_btn3,n_clicks_btn4):
     elif button_id == "btn-duration":
         fig = px.scatter(df, x='Date', y = 'Minutes', range_x=["2023-9-01", "2026-06-30"],title="Duration of runs starting from October 2023")
         fig.update_xaxes(dtick="M3")
+    elif button_id == "btn-pace":
+        fig = px.scatter(df, x='Date', y = 'Average Pace', title="Average pace tracked starting from September 2024",range_x=["2023-9-01", "2026-06-30"])
+        fig.update_layout(yaxis_title="Average Pace (Minutes per Mile)")
     else:
         fig = px.scatter(df,x='Date',y='Miles',title="Complete Tracked Mileage", hover_data= {"Date": "|%B %d, %Y"})
 
