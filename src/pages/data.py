b53685d0f0e2b43b651b52d72036744a3da17317
Ronnie <ronmabii@gmail.com>
2026-07-07 18:53:22 -0400

    added public garmin race links

--- src/pages/data.py
+++ src/pages/data.py
@@ -19,6 +19,7 @@ layout = html.Div(
             columnDefs=[{"field":"Date"}, {"field":"Activity Type"}, {"field":"Miles"}, {"field":"Duration(Mins)", "headerName": "Time"}, {"field":"Average Pace"}
                         , {"field":"Average Heart Rate"}, {"field":"Average Run Cadence"}, {"field":"Average Stride Length"} ],
             columnSize="autoSize"
+            
         )
     ]
 )
