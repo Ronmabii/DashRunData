c0de6dfa6d7fce4a1a8a48f503bb8ab963bc9744
Ronnie <ronmabii@gmail.com>
2026-07-07 16:49:35 -0400

    caps matter for files again

--- src/pages/races.py
+++ src/pages/races.py
@@ -7,7 +7,7 @@ from pathlib import Path
 dash.register_page(__name__)
 
 project_root = Path(__file__).parents[2]
-csvPath = project_root/ 'data'/'races.csv'
+csvPath = project_root/ 'data'/'Races.csv'
 
 df = pd.read_csv(csvPath)
 df['Date'] = pd.to_datetime(df['Date']).dt.date
