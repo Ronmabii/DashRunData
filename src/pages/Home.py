2a7b2edd3fd4ef0224b5bd5c91c774251196ea14
Ronnie <ronmabii@gmail.com>
2026-08-05 20:45:45 -0400

    fixed hover fomatting for duration and pace

--- src/pages/Home.py
+++ src/pages/Home.py
@@ -15,8 +15,8 @@ df = pd.read_csv(csvPath)
 
 df[["Date","Week (Monday)"]] = df[["Date","Week (Monday)"]].apply(pd.to_datetime) # needs this to be recognized as a continuous timeline, otherwise compressed(str to datetime)
 
-df['Duration(Mins)'] = pd.to_timedelta(df['Duration(Mins)']) 
-df["Minutes"] = df["Duration(Mins)"].dt.total_seconds() /60 # might be easier to convert to seconds in csv / polars has to_minutes
+df['Duration(Mins)(DT)'] = pd.to_timedelta(df['Duration(Mins)'])  # had to make DT column so original Duration(Mins) could be used for hover
+df["Minutes"] = df["Duration(Mins)(DT)"].dt.total_seconds() /60 # might be easier to convert to seconds in csv / polars has to_minutes
 
 def pace_to_float(pace): # change 10:30 min/mile format into floats (10.5) next time for easier time
     if pd.isna(pace): # nulls
@@ -24,7 +24,7 @@ def pace_to_float(pace): # change 10:30 min/mile format into floats (10.5) next 
     minutes, seconds = map(int, pace.split(':'))
     return minutes + (seconds / 60.0)
 
-df["Average Pace"] = df["Average Pace"].apply(pace_to_float)
+df["Average Pace (Float)"] = df["Average Pace"].apply(pace_to_float)
 
 weeklyMileageSum = (
     df.groupby("Week (Monday)", as_index=False)["Miles"] # dff["Miles"].sum() without as_index wrong column entered into fig
@@ -32,7 +32,10 @@ weeklyMileageSum = (
 )
 
 dffHR = df[df["Average Heart Rate"] > 120] # super low ones might be walks
-dffPace = df[df["Average Pace"] < 20] # above 20min/mile is complete walks
+dffPace = df[df["Average Pace (Float)"] < 20] # above 20min/mile is complete walks
+
+# print(df.query("`Average Heart Rate` < 120"))
+# print(df.query("`Average Pace` > 20"))
 
 layout = html.Div(
     [
@@ -64,10 +67,10 @@ def func(*args): # originally seperate buttons (n_clicks_btn1,...)
     elif button_id == "btn-heart":
         fig = px.scatter(dffHR, x='Date', y = 'Average Heart Rate',color="Activity Type", title="Average Heart Rate tracked starting from September 2024")
     elif button_id == "btn-duration":
-        fig = px.scatter(df, x='Date', y = 'Minutes', range_x=["2023-9-01", "2026-06-30"],title="Duration of runs starting from November 2023")
+        fig = px.scatter(df, x='Date', y = 'Minutes', range_x=["2023-9-01", "2026-06-30"],title="Duration of runs starting from November 2023", hover_data={"Minutes": False, "Duration(Mins)":True })
         fig.update_xaxes(dtick="M3")
     elif button_id == "btn-pace":
-        fig = px.scatter(dffPace, x='Date', y = 'Average Pace', title="Average pace tracked starting from November 2023",range_x=["2023-9-01", "2026-06-30"])
+        fig = px.scatter(dffPace, x='Date', y = 'Average Pace (Float)', title="Average pace tracked starting from November 2023",range_x=["2023-9-01", "2026-06-30"], hover_data={"Average Pace (Float)": False, "Average Pace":True })
         fig.update_layout(yaxis_title="Average Pace (Minutes per Mile)")
     else:
         fig = px.scatter(df,x='Date',y='Miles',title="Daily Running Mileage", hover_data= {"Date": "|%B %d, %Y"})
