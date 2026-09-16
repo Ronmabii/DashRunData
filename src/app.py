b3eec06c8f4ce339088e71c1bb424ed6d368d072
Ronnie LaptopL <ronmabii@gmail.com>
2026-09-16 15:11:03 -0400

    testing laptop push pychache(should be nothing), and a comment commit dont forget to pull on desktop first

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import dash
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 
-
+#laptop commit test
 app = Dash(__name__,use_pages=True, external_stylesheets=[dbc.themes.SUPERHERO]) # must run from app.py
 server = app.server
 
